997f9382c65dbcafbe7ad5dd41bba0ceacfcca39
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2022-11-10 02:43:37 +0100

    Update .rpush.sh
    set max age to 7d

--- .rpush.sh
+++ .rpush.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 if ! pgrep -x "rclone" > /dev/null
 then
-	rclone copy --bwlimit off:off gdrive:sync /home/jkt/sync
-	rclone sync --bwlimit off:off /home/jkt/sync gdrive:sync
+	rclone copy --bwlimit off:off --max-age 7d gdrive:sync /home/jkt/sync
+	rclone sync --bwlimit off:off --max-age 7d /home/jkt/sync gdrive:sync
 	XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -h int:transient:1 -u normal Rclone "gDrive Synced"
 fi
+
 
