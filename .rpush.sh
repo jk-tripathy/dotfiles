6699cc7e291b3ae43e0b8db902a25677c71807c8
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2022-11-10 02:38:24 +0100

    Update .rpush.sh
    - copy from remote first then sync.
    - using notify-send

--- .rpush.sh
+++ .rpush.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-rclone sync -v -L $1 ssDrive:$1
+if ! pgrep -x "rclone" > /dev/null
+then
+	rclone copy --bwlimit off:off gdrive:sync /home/jkt/sync
+	rclone sync --bwlimit off:off /home/jkt/sync gdrive:sync
+	XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -h int:transient:1 -u normal Rclone "gDrive Synced"
+fi
+
