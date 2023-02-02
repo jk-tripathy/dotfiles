ee473096f3a4988ec501f68bb9291aba5bd115d2
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2023-02-02 22:44:28 +0100

    fixed bug with root dir exec

--- runpy.sh
+++ runpy.sh
@@ -4,7 +4,7 @@ subdir=$(cut -d'/' -f3- <<<"$dirname")
 rsync -avuzh ~/jarvis/$projname/ im:project/$projname/
 if [ "$1" == "-v" ]
 then
-    ssh im "cd project/$projname; export PYTHONPATH=.:$PYTHONPATH; venv/bin/python $subdir/$2"
+    ssh im "cd project/$projname; export PYTHONPATH=.:$PYTHONPATH; venv/bin/python .$subdir/$2"
 else
-    ssh im "cd project/$projname; export PYTHONPATH=.:$PYTHONPATH; python3 $subdir/$1"
+    ssh im "cd project/$projname; export PYTHONPATH=.:$PYTHONPATH; python3 .$subdir/$1"
 fi
