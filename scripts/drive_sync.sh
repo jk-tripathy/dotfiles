3438b021c8d5dcbd7d7dbbc235e74b41959e95a3
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2023-10-02 06:06:30 +0200

    fix path and add notification

--- scripts/drive_sync.sh
+++ scripts/drive_sync.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # yeet2drive
-rclone sync -uM --fast-list --transfers=5 --checkers=5 Sync/ gdrive:Sync/
+rclone sync -uM --fast-list --transfers=5 --checkers=5 ~/Sync/ gdrive:Sync/
+notify-send -t 1000 "Synced files to GDrive"
