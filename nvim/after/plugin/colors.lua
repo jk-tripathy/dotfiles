624f1898bd3317387e2d4fee1d0a9a1f1b271465
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2023-10-24 05:55:24 +0200

    removed bug in nvim.colors + toggle opacity kbinds

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -4,22 +4,4 @@ require("rose-pine").setup({
 	disable_background = true,
 })
 
-require("kanagawa").setup({
-	compile = false, -- enable compiling the colorscheme
-	undercurl = true, -- enable undercurls
-	commentStyle = { italic = true },
-	functionStyle = { bold = true },
-	keywordStyle = { italic = true },
-	statementStyle = { bold = true },
-	typeStyle = {},
-	transparent = true,
-	dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-	terminalColors = true, -- define vim.g.terminal_color_{0,17}
-	theme = "dragon", -- Load "wave" theme when 'background' option is not set
-	background = { -- map the value of 'background' option to a theme
-		dark = "wave", -- try "dragon" !
-		light = "lotus",
-	},
-})
-
 vim.cmd.colorscheme("rose-pine")
