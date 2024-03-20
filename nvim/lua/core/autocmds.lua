2071d9ec4de75f096dac93e59291e9747fe4b1c4
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2024-03-20 06:48:42 +0100

    feat: finally added some autocmds

---
+++ nvim/lua/core/autocmds.lua
@@ -0,0 +1,16 @@
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+    group = vim.api.nvim_create_augroup("resize_splits", { clear = true }),
+    callback = function()
+        local current_tab = vim.fn.tabpagenr()
+        vim.cmd("tabdo wincmd =")
+        vim.cmd("tabnext " .. current_tab)
+    end,
+})
