8551046f4cd6097a2e946f81b2b53ee532d8b102
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2024-03-20 07:18:47 +0100

    feat: no show fmter errors

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -49,7 +49,7 @@ local M = {
                 },
             },
         },
-
+        notify_on_error = false,
         format_on_save = {
             timeout_ms = 500,
             lsp_fallback = true,
