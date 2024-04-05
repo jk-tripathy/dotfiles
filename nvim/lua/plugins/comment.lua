973bce77c2768aac95bca10bb09e9d2c4a0d5d5f
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2024-04-05 08:46:52 +0200

    fix: not sure why i changed this

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -2,7 +2,7 @@ local M = {
     "numToStr/Comment.nvim",
     event = "BufReadPost",
     opts = {
-        opleader = { line = "/", block = "b/" },
+        opleader = { line = ",", block = "b," },
     },
 }
 
