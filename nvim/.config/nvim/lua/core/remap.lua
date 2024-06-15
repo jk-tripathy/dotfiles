76516d48ba6b67caab8e0d2caa2448c138fadd3f
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2024-06-15 13:08:28 +0200

    fix: bug in remap

--- nvim/.config/nvim/lua/core/remap.lua
+++ nvim/.config/nvim/lua/core/remap.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 vim.keymap.set("n", "E", "$")
-vim.keymap.set("n", "B", "0w")
+vim.keymap.set("n", "B", "0")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
