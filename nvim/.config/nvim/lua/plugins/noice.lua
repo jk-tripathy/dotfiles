63bd50935aab684be52df5a2066d09a749bd674e
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2024-06-16 13:20:35 +0200

    fix: cmd input to mid of screen

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -11,7 +11,7 @@ local M = {
         },
         presets = {
             bottom_search = true,
-            command_palette = true,
+            command_palette = false,
             long_message_to_split = true,
             inc_rename = false,
             lsp_doc_border = false,
