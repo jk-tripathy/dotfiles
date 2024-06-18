b9b3827bfee704339f0749f32a4652c21931117e
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2024-06-18 12:08:01 +0200

    fix: i will need to move left, fuckign idiot

--- nvim/.config/nvim/lua/core/remap.lua
+++ nvim/.config/nvim/lua/core/remap.lua
@@ -18,6 +18,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<M-k>", "<C-w>k")
 vim.keymap.set("n", "<M-h>", "<C-w>h")
 vim.keymap.set("n", "<M-j>", "<C-w>j")
+vim.keymap.set("n", "<M-l>", "<C-w>l")
 vim.keymap.set("n", "<M-t>", "<C-w>T")
 
 -- window resizing
