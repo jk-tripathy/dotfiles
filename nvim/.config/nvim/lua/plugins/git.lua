f02e3fdc0300b0142570c1fa4e29244c6938a6f9
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2024-07-12 11:04:41 +0200

    feat: set upstream easily

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -22,14 +22,9 @@ local M = {
                         vim.cmd.Git("push")
                     end, opts)
 
-                    -- rebase always
-                    vim.keymap.set("n", "<leader>P", function()
-                        vim.cmd.Git({ "pull", "--rebase" })
-                    end, opts)
-
                     -- NOTE: It allows me to easily set the branch i am pushing and any tracking
                     -- needed if i did not set the branch up correctly
-                    vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
+                    vim.keymap.set("n", "<leader>P", ":Git push -u origin ", opts)
                 end,
             })
         end,
