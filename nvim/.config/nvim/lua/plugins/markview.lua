4a321aea970dd59bece80072cdac19544545ed81
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2024-08-21 14:19:20 +0200

    feat: mark down inline viewing

---
+++ nvim/.config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,10 @@
+local M = {
+    "OXY2DEV/markview.nvim",
+    ft = "markdown",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons"
+    }
+}
+
+return M
