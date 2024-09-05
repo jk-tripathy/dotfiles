378b4d48e284db6860454519ed039f6fc36408f2
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2024-09-05 10:15:51 +0200

    fix: bad snip kbinds

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -43,16 +43,6 @@ local M = {
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<CR>"] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete({}),
-                ["<C-l>"] = cmp.mapping(function()
-                    if luasnip.expand_or_locally_jumpable() then
-                        luasnip.expand_or_jump()
-                    end
-                end, { "i", "s" }),
-                ["<C-h>"] = cmp.mapping(function()
-                    if luasnip.locally_jumpable(-1) then
-                        luasnip.jump(-1)
-                    end
-                end, { "i", "s" }),
             }),
             sources = {
                 { name = "nvim_lsp" },
