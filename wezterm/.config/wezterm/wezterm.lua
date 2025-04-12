9f71788ecdee5f6c567c76855bafdcf1cdaa999a
Jatin Karthik Tripathy <jkt@amazon.de>
2025-04-12 14:47:34 +0200

    chore: formatting

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 local act = wezterm.action
 
@@ -12,85 +12,85 @@ config.font = wezterm.font("Iosevka Nerd Font")
 
 -- Apperance
 config.cursor_blink_rate = 0
-config.window_decorations = 'RESIZE'
+config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-    left = 10,
-    right = 10,
-    top = 10,
-    bottom = 10,
+	left = 10,
+	right = 10,
+	top = 10,
+	bottom = 10,
 }
 config.colors = {
-    cursor_bg = "white",
-    cursor_border = "white",
+	cursor_bg = "white",
+	cursor_border = "white",
 }
 config.macos_window_background_blur = 40
 
 -- keybinds
 config.keys = {
-    -- close pane
-    {
-        key = 'w',
-        mods = 'CTRL',
-        action = act.CloseCurrentPane { confirm = false },
-    },
-    -- splits
-    {
-        key = '_',
-        mods = 'SHIFT|ALT',
-        action = act.SplitVertical {
-            domain = 'CurrentPaneDomain'
-        },
-    },
-    {
-        key = '+',
-        mods = 'SHIFT|ALT',
-        action = act.SplitHorizontal {
-            domain = 'CurrentPaneDomain'
-        },
-    },
-    -- pane movement
-    {
-        key = 'H',
-        mods = 'SHIFT|ALT',
-        action = act.ActivatePaneDirection 'Left',
-    },
-    {
-        key = 'J',
-        mods = 'SHIFT|ALT',
-        action = act.ActivatePaneDirection 'Down',
-    },
-    {
-        key = 'K',
-        mods = 'SHIFT|ALT',
-        action = act.ActivatePaneDirection 'Up',
-    },
-    {
-        key = 'L',
-        mods = 'SHIFT|ALT',
-        action = act.ActivatePaneDirection 'Right',
-    },
-    -- pane resize
-    {
-        key = ',',
-        mods = 'CTRL|ALT',
-        action = act.AdjustPaneSize { 'Left', 5 },
-    },
-    {
-        key = '.',
-        mods = 'CTRL|ALT',
-        action = act.AdjustPaneSize { 'Right', 5 },
-    },
-    {
-        key = '<',
-        mods = 'CTRL|SHIFT|ALT',
-        action = act.AdjustPaneSize { 'Up', 5 },
-    },
-    {
-        key = '>',
-        mods = 'CTRL|SHIFT|ALT',
-        action = act.AdjustPaneSize { 'Down', 5 },
-    },
+	-- close pane
+	{
+		key = "w",
+		mods = "CTRL",
+		action = act.CloseCurrentPane({ confirm = false }),
+	},
+	-- splits
+	{
+		key = "_",
+		mods = "SHIFT|ALT",
+		action = act.SplitVertical({
+			domain = "CurrentPaneDomain",
+		}),
+	},
+	{
+		key = "+",
+		mods = "SHIFT|ALT",
+		action = act.SplitHorizontal({
+			domain = "CurrentPaneDomain",
+		}),
+	},
+	-- pane movement
+	{
+		key = "H",
+		mods = "SHIFT|ALT",
+		action = act.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "J",
+		mods = "SHIFT|ALT",
+		action = act.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "K",
+		mods = "SHIFT|ALT",
+		action = act.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "L",
+		mods = "SHIFT|ALT",
+		action = act.ActivatePaneDirection("Right"),
+	},
+	-- pane resize
+	{
+		key = ",",
+		mods = "CTRL|ALT",
+		action = act.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		key = ".",
+		mods = "CTRL|ALT",
+		action = act.AdjustPaneSize({ "Right", 5 }),
+	},
+	{
+		key = "<",
+		mods = "CTRL|SHIFT|ALT",
+		action = act.AdjustPaneSize({ "Up", 5 }),
+	},
+	{
+		key = ">",
+		mods = "CTRL|SHIFT|ALT",
+		action = act.AdjustPaneSize({ "Down", 5 }),
+	},
 }
 
 -- WSL 2
