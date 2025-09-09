a8fa093e860a29e9d511692db740c74133dfd717
Jatin Karthik Tripathy <jkt@amazon.de>
2025-09-09 13:12:11 +0000

    feat: added quick setup script

---
+++ setup_devdsk.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+cd 
+
+#Amazon Stuff
+# toolbox install axe
+# source $HOME/.zshrc
+# axe init builder-tools
+# toolbox install q
+# source $HOME/.zshrc
+# q login --license pro --identity-provider https://amzn.awsapps.com/start --region us-east-1
+
+# q Stuff
+# toolbox install mcp-registry
+# mcp-registry install builder-mcp
+# mcp-registry install amzn-mcp
+
+# Nvim
+# wget https://github.com/neovim/neovim-releases/releases/download/v0.11.0/nvim-linux-x86_64.tar.gz && tar -xzvf ~/nvim-linux-x86_64.tar.gz
+# mv nvim-linux-x86_64 nvim
+
+# Zellij
+# wget https://github.com/zellij-org/zellij/releases/download/v0.43.1/zellij-no-web-x86_64-unknown-linux-musl.tar.gz && tar -xzvf zellij-no-web-x86_64-unknown-linux-musl.tar.gz
+# mv zellij .local/bin
+
+# Lazygit
+# wget https://github.com/jesseduffield/lazygit/releases/download/v0.55.0/lazygit_0.55.0_linux_x86_64.tar.gz
+# tar -xzvf ~/lazygit_0.55.0_linux_x86_64.tar.gz
+# mv lazygit .local/bin
+
+# GitHub SSH Setup 
+# ssh-keygen -t ed25519 -C "jatinkarthikt@gmail.com"
+# eval "$(ssh-agent -s)"
+# ssh-add ~/.ssh/id_ed25519
+# cat ~/.ssh/id_ed25519.pub
+
+# Dotfiles
+# git clone git@github.com:jk-tripathy/dotfiles.git
+# cd .config
+# ln -s $HOME/dotfiles/nvim nvim
+# ln -s $HOME/dotfiles/zellij zellij
+
+# Paths and Aliases
+# echo '# custom paths and aliases' >> $HOME/.zshrc
+# echo 'export PATH=$HOME/nvim/bin:$PATH' >> $HOME/.zshrc
+# echo 'export PATH=$HOME/.local/bin:$PATH' >> $HOME/.zshrc
+# echo 'alias mw="mwinit -o -s"' >> $HOME/.zshrc
+# echo 'alias nv="nvim"' >> $HOME/.zshrc
+# echo 'alias z="zellij"' >> $HOME/.zshrc
+# echo 'alias za="zellij a"' >> $HOME/.zshrc
+# echo 'alias ls="ls -a"' >> $HOME/.zshrc
+# echo 'alias gg="lazygit"' >> $HOME/.zshrc
+
+#zsh autocomplete
+# git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
+# sed -i '1isource ~/zsh-autocomplete/zsh-autocomplete.plugin.zsh' ~/.zshrc
+
+# zsh syntax highlighting
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+# echo "source $HOME/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+
+# powerlevel10k
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+# echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+# rm -rf *.tar.gz
+# source ~/.zshrc
+
